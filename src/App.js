39a7b9feb6d2ca78c6821f448825a67106326fa4
Saoa35 <saoamah@gmail.com>
2022-10-20 21:44:49 +0200

    created arr

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import { Card } from "./components/Card";
 import Cart from "./components/Cart";
 import Header from "./components/Header";
 
+const arr = [
+  {title:'Fighter Aircraft Hawker Tempest', img:'/img/Goods/1.jpg', price:'200 uah'},
+  {title:'Messerschmitt Bf.110 Night Fighter', img:'/img/Goods/2.jpg', price:'300 uah'},
+  {title:'Bomber Boeing B-29 Superfortress', img:'/img/Goods/3.jpg', price:'500 uah'},
+  {title:'Carrier-based Fighter Grumman F6F Hellcat', img:'/img/Goods/4.jpg', price:'350 uah'}
+]
+
 function App() {
   return (
     <div className="wrapper">
@@ -22,58 +29,10 @@ function App() {
        
         <div className='goods'>
 
-          <Card />
+          {arr.map((obj, index) => 
+            <Card key={index} title={obj.title} img={obj.img} price={obj.price} />
+           )}
 
-          {/* <div className="card">
-            <div className='favorite'>
-              <img src='/img/unliked.svg' alt="Unliked" title="Set as Favorite"/>
-            </div>
-            <img width={170} height={140} style={{borderRadius:'10px'}}  src="/img/Goods/2.jpg" alt="Goods"/>
-            <h5>Messerschmitt Bf.110 Night Fighter</h5>
-            <div className='cardBottom'>
-              <div className="cardPrice">
-                <span>Price:</span>
-                <b>300 uah</b>
-              </div>
-              <button className='button'>
-                <img src='/img/plus.svg' width={11} height={11} alt='Plus' title="Add to Cart"/>
-              </button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className='favorite'>
-              <img src='/img/unliked.svg' alt="Unliked" title="Set as Favorite"/>
-            </div>
-            <img width={170} height={140} style={{borderRadius:'10px'}}  src="/img/Goods/3.jpg" alt="Goods"/>
-            <h5>Bomber Boeing B-29 Superfortress</h5>
-            <div className='cardBottom'>
-              <div className="cardPrice">
-                <span>Price:</span>
-                <b>500 uah</b>
-              </div>
-              <button className='button'>
-                <img src='/img/plus.svg' width={11} height={11} alt='Plus' title="Add to Cart"/>
-              </button>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className='favorite'>
-              <img src='/img/unliked.svg' alt="Unliked" title="Set as Favorite"/>
-            </div>
-            <img width={170} height={140} style={{borderRadius:'10px'}}  src="/img/Goods/4.jpg" alt="Goods"/>
-            <h5>Carrier-based Fighter Grumman F6F Hellcat</h5>
-            <div className='cardBottom'>
-              <div className="cardPrice">
-                <span>Price:</span>
-                <b>350 uah</b>
-              </div>
-              <button className='button'>
-                <img src='/img/plus.svg' width={11} height={11} alt='Plus' title="Add to Cart"/>
-              </button>
-            </div>
-          </div> */}
 
         </div>
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
 
-export function Card() {
+export function Card({ img, title, price }) {
 
   return(
     <div className="card">
       <div className='favorite'>
         <img src='/img/unliked.svg' alt="Unliked" title="Set as Favorite"/>
       </div>
-      <img width={170} height={140} style={{borderRadius:'10px'}}  src="/img/Goods/1.jpg" alt="Goods"/>
-      <h5>Fighter Aircraft Hawker Tempest</h5>
+      <img width={170} height={140} style={{borderRadius:'10px'}}  src={img} alt="Goods"/>
+      <h5>{title}</h5>
       <div className='cardBottom'>
         <div className="cardPrice">
           <span>Price:</span>
-          <b>200 uah</b>
+          <b>{price}</b>
         </div>
         <button className='button'>
           <img src='/img/plus.svg' width={11} height={11} alt='Plus' title="Add to Cart"/>
