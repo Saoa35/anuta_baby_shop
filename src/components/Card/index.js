6361cbf33cdf98d7ee674c5476f92447052bb6dc
Saoa35 <saoamah@gmail.com>
2022-10-21 20:21:51 +0200

    created card.mpdules.scss

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,8 +1,9 @@
+import styles from './Card.module.scss';
 
 export function Card(props) {
 
   return(
-    <div className="card">
+    <div className="styles.card">
       <div className='favorite'>
         <img src='/img/unliked.svg' alt="Unliked" title="Set as Favorite"/>
       </div>
