7112c88ee6639ed886614cf78fdbd756925296f0
Saoa35 <saoamah@gmail.com>
2022-10-22 11:42:44 +0200

    del plus button from card comp.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,6 +5,10 @@ export function Card(props) {
 
   const [isAdded, setIsAdded] = useState(false);
 
+  const onClickPlus = () => {
+    setIsAdded(!isAdded);
+  };
+
   return(
     <div className={styles.card}>
       <div className={styles.favorite} onClick={props.onClickFavorit}>
@@ -17,9 +21,7 @@ export function Card(props) {
           <span>Price:</span>
           <b>{props.price} UAH</b>
         </div>
-        <button className={styles.button} onClick={props.onClickPlus}>
-          <img src='/img/plus.svg' width={11} height={11} alt='Plus' title="Add to Cart"/>
-        </button>
+          <img onClick={onClickPlus} src={isAdded ? '/img/btn-checked.svg' : '/img/plus.svg'} width={11} height={11} alt='Plus' title="Add to Cart"/>
       </div>
       </div>
   )
