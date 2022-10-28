c4a85ba9ca8136e2209c4aff1d1f7471b707ecfe
Saoa35 <saoamah@gmail.com>
2022-10-28 14:32:59 +0200

    + ContentLoader to Card render

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -19,7 +19,7 @@ export function Card({ id, title, img, price, onPlus, onFavorite, isFavorite = f
 
   return(
     <div className={styles.card}>
-      <div className={styles.favorite} onClick={onClickFavorite}>
+      {/* <div className={styles.favorite} onClick={onClickFavorite}>
         <img src={isLiked ? '/img/liked.svg' : '/img/unliked.svg'} 
              alt='Heart' 
              title={!isLiked ? "Set as Favorite" : 'Delete from Favorite'}/>
@@ -36,7 +36,24 @@ export function Card({ id, title, img, price, onPlus, onFavorite, isFavorite = f
              src={isAdded ? '/img/btn-checked.svg' : '/img/btn-plus.svg'} 
              alt='Plus' 
              title={!isAdded ? "Add to Cart" : 'Delete from Cart'}/>
-      </div>
+      </div> */}
+
+
+    <ContentLoader 
+          speed={0}
+          width={210}
+          height={295}
+          viewBox="0 0 210 295"
+          backgroundColor="#f3f3f3"
+          foregroundColor="#ecebeb">
+            
+          <rect x="0" y="0" rx="10" ry="10" width="170" height="140" /> 
+          <rect x="92" y="199" rx="0" ry="0" width="1" height="0" /> 
+          <rect x="-1" y="165" rx="0" ry="0" width="170" height="20" /> 
+          <rect x="0" y="195" rx="0" ry="0" width="100" height="20" /> 
+          <rect x="0" y="239" rx="0" ry="0" width="80" height="25" /> 
+          <rect x="134" y="234" rx="5" ry="5" width="32" height="32" />
+      </ContentLoader>
     </div>
   )
 }
