0783f9070b5674ce47437b6d32e97e4ae61a5f3a
Saoa35 <saoamah@gmail.com>
2022-12-06 22:37:04 +0100

    changed shop name

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,43 +2,65 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useCart } from "../hooks/useCart";
 
-
 function Header(props) {
-
   const { totalPrice } = useCart();
-    
-    return(
-        <header>
-        
-          <div className="headerLeft">
-            <Link to='/'>
-              <img src='/img/logo.png' width={30} heigth={30} style={{borderRadius:'20px'}} alt='logo'/>
-            </Link>
-            <div className="headerInfo">
-              <h3>Anuta Baby Shop</h3>
-              <p>The best goods for adults and children</p>
-            </div>
-          </div>
-        
-        
-        <ul className="headerRight">
-          <li>
-            <img onClick={props.onClickCart} src="/img/cart.svg" width={25} height={25} alt='cart' title="Open Cart"/>
-            <span style={{margin:'0 20px'}}>{totalPrice} UAH</span>
-          </li>
-          <li>
-           <Link to="favorites">
-            <img src="/img/heart.svg" style={{marginRight:'20px'}} width={25} height={25} alt='heart' title="Open Favorites"/>
-           </Link>
-          </li>
-          <li>
-            <Link to="/orders">
-              <img src="/img/user.svg" width={25} height={25} alt='user' title="Your Orders"/>
-            </Link>
-          </li>
-        </ul>
-      </header>
-    )
+
+  return (
+    <header>
+      <div className="headerLeft">
+        <Link to="/">
+          <img
+            src="/img/logo.png"
+            width={30}
+            heigth={30}
+            style={{ borderRadius: "20px" }}
+            alt="logo"
+          />
+        </Link>
+        <div className="headerInfo">
+          <h3>Model Shop</h3>
+          <p>The best goods for adults and children</p>
+        </div>
+      </div>
+
+      <ul className="headerRight">
+        <li>
+          <img
+            onClick={props.onClickCart}
+            src="/img/cart.svg"
+            width={25}
+            height={25}
+            alt="cart"
+            title="Open Cart"
+          />
+          <span style={{ margin: "0 20px" }}>{totalPrice} UAH</span>
+        </li>
+        <li>
+          <Link to="favorites">
+            <img
+              src="/img/heart.svg"
+              style={{ marginRight: "20px" }}
+              width={25}
+              height={25}
+              alt="heart"
+              title="Open Favorites"
+            />
+          </Link>
+        </li>
+        <li>
+          <Link to="/orders">
+            <img
+              src="/img/user.svg"
+              width={25}
+              height={25}
+              alt="user"
+              title="Your Orders"
+            />
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
