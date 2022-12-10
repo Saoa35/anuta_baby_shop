6d3b56442f429087e305a60f9e6dd1d5ead6041a
Saoa35 <saoamah@gmail.com>
2022-12-10 18:49:31 +0100

    fixet gramar mistake in catch method

--- src/App.js
+++ src/App.js
@@ -8,74 +8,79 @@ import Home from "./pages/Home";
 import AppContext from "./context";
 import Orders from "./pages/Orders";
 
-
 function App() {
-
-  const url = 'https://63540251e64783fa827d56c7.mockapi.io';
+  const url = "https://6394c3f44df9248eadac2772.mockapi.io";
 
   const [cartOpened, setCartOpened] = useState(false);
   const [items, setItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
-  const [serchValue, setSerchValue] = useState('');
-  const [favorites, setFavorites] =useState([]);
+  const [serchValue, setSerchValue] = useState("");
+  const [favorites, setFavorites] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    async function fetchData () {
+    async function fetchData() {
       try {
         // const cartResponse = await axios.get(`${url}/cart`);
         // const favoritesResponse = await axios.get(`${url}/favorites`);
         // const itemsResponse = await axios.get(`${url}/items`);
 
-        const [cartResponse, favoritesResponse, itemsResponse] = await Promise.all([
-          axios.get(`${url}/cart`),
-          axios.get(`${url}/favorites`),
-          axios.get(`${url}/items`),
-        ]);
-    
+        const [cartResponse, favoritesResponse, itemsResponse] =
+          await Promise.all([
+            axios.get(`${url}/cart`),
+            axios.get(`${url}/favorites`),
+            axios.get(`${url}/items`),
+          ]);
+
         setLoading(false);
         setCartItems(cartResponse.data);
         setFavorites(favoritesResponse.data);
         setItems(itemsResponse.data);
       } catch (error) {
-        console.log(error.mesage);
+        console.log(error.message);
       }
     }
-    
-    fetchData ();
 
-  },[]);
+    fetchData();
+  }, []);
 
   const onAddToCart = async (obj) => {
     try {
-      const findItem = cartItems.find(item => Number(item.parentId) === Number(obj.id));
-      if(findItem) {
-        setCartItems(prev => prev.filter(item => Number(item.parentId) !== Number(obj.id)));
+      const findItem = cartItems.find(
+        (item) => Number(item.parentId) === Number(obj.id)
+      );
+      if (findItem) {
+        setCartItems((prev) =>
+          prev.filter((item) => Number(item.parentId) !== Number(obj.id))
+        );
         await axios.delete(`${url}/cart/${findItem.id}`);
       } else {
-        setCartItems(prev => [...prev, obj]);
-        const { data }  = await axios.post(`${url}/cart`, obj);
-        setCartItems(prev => prev.map(item => {
-          if(item.parentId === data.parentId) {
-            return {
-              ...item,
-              id: data.id
-            };
-          }
-          return item;
-        }));
-      // setCartItems([...cartItems, obj]);
+        setCartItems((prev) => [...prev, obj]);
+        const { data } = await axios.post(`${url}/cart`, obj);
+        setCartItems((prev) =>
+          prev.map((item) => {
+            if (item.parentId === data.parentId) {
+              return {
+                ...item,
+                id: data.id,
+              };
+            }
+            return item;
+          })
+        );
+        // setCartItems([...cartItems, obj]);
       }
     } catch (error) {
       console.log(error.mesage);
     }
   };
 
-
   const onRemoveFromCart = (id) => {
     try {
       axios.delete(`${url}/cart/${id}`);
-      setCartItems(prev => prev.filter(item => Number(item.id) !== Number(id)));
+      setCartItems((prev) =>
+        prev.filter((item) => Number(item.id) !== Number(id))
+      );
     } catch (error) {
       console.log(error.mesage);
     }
@@ -87,12 +92,14 @@ function App() {
 
   const onAddToFavorite = async (obj) => {
     try {
-      if(favorites.find(object => Number(object.id) === Number(obj.id))) {
+      if (favorites.find((object) => Number(object.id) === Number(obj.id))) {
         axios.delete(`${url}/favorites/${obj.id}`);
-          setFavorites(prev => prev.filter(item => Number(item.id) !== Number(obj.id)));
+        setFavorites((prev) =>
+          prev.filter((item) => Number(item.id) !== Number(obj.id))
+        );
       } else {
         const { data } = await axios.post(`${url}/favorites`, obj);
-      setFavorites(prev => [...prev, data]);
+        setFavorites((prev) => [...prev, data]);
       }
     } catch (error) {
       console.error(error);
@@ -100,46 +107,54 @@ function App() {
   };
 
   const isItemAdded = (id) => {
-    return cartItems.some(obj => Number(obj.parentId) === Number(id));
-  }
+    return cartItems.some((obj) => Number(obj.parentId) === Number(id));
+  };
 
   return (
-    <AppContext.Provider value={{ 
-        items, 
-        cartItems, 
-        favorites, 
-        isItemAdded, 
+    <AppContext.Provider
+      value={{
+        items,
+        cartItems,
+        favorites,
+        isItemAdded,
         onAddToFavorite,
         onAddToCart,
-        setCartOpened, 
-        setCartItems }}>
-          
-    <div className="wrapper">
+        setCartOpened,
+        setCartItems,
+      }}
+    >
+      <div className="wrapper">
+        {cartOpened && (
+          <Cart
+            onCloseCart={() => setCartOpened(false)}
+            items={cartItems}
+            onRemove={onRemoveFromCart}
+            opened={cartOpened}
+          />
+        )}
 
-      {cartOpened && <Cart onCloseCart={() => setCartOpened(false)} items={cartItems} onRemove={onRemoveFromCart} opened={cartOpened} />}
-
-      <Header onClickCart={() => setCartOpened(true)} />
+        <Header onClickCart={() => setCartOpened(true)} />
 
         <Route path="/" exact>
-        <Home 
-          serchValue={serchValue} 
-          items={items}
-          cartItems={cartItems}
-          onChangeInput={onChangeInput}
-          onAddToFavorite={onAddToFavorite}
-          onAddToCart={onAddToCart}
-          loading={loading} />
-      </Route> 
-      
-      <Route path="/favorites" exact>
-        <Favorites />
-      </Route>
+          <Home
+            serchValue={serchValue}
+            items={items}
+            cartItems={cartItems}
+            onChangeInput={onChangeInput}
+            onAddToFavorite={onAddToFavorite}
+            onAddToCart={onAddToCart}
+            loading={loading}
+          />
+        </Route>
 
-      <Route path="/orders" exact>
-        <Orders />
-      </Route>
+        <Route path="/favorites" exact>
+          <Favorites />
+        </Route>
 
-    </div>
+        <Route path="/orders" exact>
+          <Orders />
+        </Route>
+      </div>
     </AppContext.Provider>
   );
 }
